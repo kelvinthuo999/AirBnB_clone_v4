62e5cd0b26ae7a558f5d2648fc392a0c53e6cdb6
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-02-10 00:18:42 -0800

    create: checkbox and an array to hold the status

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  const selectedAmenities = [];
+
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      selectedAmenities.push(amenityId);
+    } else {
+      const index = selectedAmenities.indexOf(amenityId);
+      if (index !== -1) {
+        selectedAmenities.splice(index, 1);
+      }
+    }
+
+    $('#amenities h4').text(selectedAmenities.join(', '));
+  });
+);
